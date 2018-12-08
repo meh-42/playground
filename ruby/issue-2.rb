f46adacd54e3205f6643ddfe017831c385a0f4d1
Edivad <psst@edivad.art>
2018-12-08 14:32:00 +0100

    Correzioni issue 2

--- ruby/issue-2.rb
+++ ruby/issue-2.rb
@@ -1,15 +1,11 @@
 def long_words_count(x)
   list= x.split
-  list.select do |word|
+  selected= list.select do |word|
   word.length > 5
   end
+  selected.count
 end
 
-x = "Nel ciel che più de la sua luce prende fu’ io e vidi cose che ridire né sa né può chi di là sù discende perché appressando sé al suo disire nostro intelletto si profonda tanto che dietro la memoria non può ire"
-n = long_words_count(x)
+x= "Nel ciel che più de la sua luce prende fu’ io e vidi cose che ridire né sa né può chi di là sù discende perché appressando sé al suo disire nostro intelletto si profonda tanto che dietro la memoria non può ire"
+n= long_words_count(x)
 puts "There are #{n} words longer than 5 characters"
-
-selected = list= x.split
-list.select do |word|
-  word.length > 5
-end
