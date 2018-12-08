923decbcd2469b5e39b121b50763046b0dd0e897
Edivad <psst@edivad.art>
2018-12-08 16:50:22 +0100

    #4 In questo modo? Non pensavo si potesse anche ad "end"

--- ruby/issue-4.rb
+++ ruby/issue-4.rb
@@ -1,8 +1,8 @@
 def long_words_count(x)
-  selected = x.split.select do |word|
+  x.split.select do |word|
     word.length > 5
-  end
+  end.count
 end
 x = "Nel ciel che più de la sua luce prende fu’ io e vidi cose che ridire né sa né può chi di là sù discende perché appressando sé al suo disire nostro intelletto si profonda tanto che dietro la memoria non può ire"
-n = long_words_count(x).count
+n = long_words_count(x)
 puts "There are #{n} words longer than 5 characters"
