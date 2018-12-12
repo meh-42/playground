ad0800730557ae5c11e340212f1c089227f0b0e5
Edivad <psst@edivad.art>
2018-12-12 17:03:27 +0100

    #5 finalmente il pc è ripartito, che ne pensi?

---
+++ ruby/issue-5.rb
@@ -0,0 +1,10 @@
+def long_words_count(x,l)
+  x.split.select do |word|
+    word.length >= l
+  end.count
+end
+
+x = "Nel ciel che più de la sua luce prende fpu’ io e vidi cose che ridire né sa né può chi di là sù discende perché appressando sé al suo disire nostro intelletto si profonda tanto che dietro la memoria non può ire"
+l = 6
+n = long_words_count(x,l)
+puts "There are #{n} words longer than #{l} characters"
