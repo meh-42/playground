9b19a3b517a188da52af1b3a61ef9c3ecfbbf9e3
Edivad <edivad@kiddo.lan>
2018-12-13 17:21:14 +0100

    #5💄 Code makeup

--- ruby/issue-5.rb
+++ ruby/issue-5.rb
@@ -1,4 +1,4 @@
-def long_words_count(x,l)
+def long_words_count(x, l)
   x.split.select do |word|
     word.length >= l
   end.count
@@ -6,5 +6,5 @@ end
 
 x = "Nel ciel che più de la sua luce prende fpu’ io e vidi cose che ridire né sa né può chi di là sù discende perché appressando sé al suo disire nostro intelletto si profonda tanto che dietro la memoria non può ire"
 l = 6
-n = long_words_count(x,l)
+n = long_words_count(x, l)
 puts "There are #{n} words longer than #{l} characters"
